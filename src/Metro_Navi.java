3c454d23d4336179de21e8dd7cdc94cf8d4c6a90
CoCo <demy176@naver.com>
2022-04-27 17:00:56 +0900

    역 정보 가져오기

--- src/Metro_Navi.java
+++ src/Metro_Navi.java
@@ -1,3 +1,4 @@
+import java.time.LocalTime;
 import java.util.*;
 import java.sql.*;
 
@@ -8,10 +9,16 @@ class subwayData {
     int lineId; //호선
     int type;   //급행역 여부
     boolean transfer;   //환승역 여부
+    int beforeStation;  //이전 역
+    int nextStation;    //다음 역
     String departureTime;   //출발 시간
     String arrivalTime; //도착 시간
     String congestion;  //혼잡도
     int congestionValue;    //혼잡도 값
+
+    void getDepartureTime() {
+        LocalTime nowTime = LocalTime.now();
+    }
 }
 
 class node {
@@ -43,25 +50,27 @@ class databaseManager {
         return conn;
     }
 
-    subwayData getStationData(String departureStation) {
+    ArrayList getStationData(String departureStation) {
          Connection conn = connectDatabase();
          java.sql.ResultSet rs = null;
-         subwayData station = new subwayData();
+         ArrayList<subwayData> station = new ArrayList<subwayData>();
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Statement stmt = conn.createStatement();
             String strQuery;
             strQuery = String.format("SELECT station_detail_id, station_name, station_code, line_id, before_station, next_station, " +
                     "station_id FROM Subway.sub_line_name_info WHERE station_name = \"%s\"", departureStation);
-            System.out.print(strQuery);
             rs = stmt.executeQuery(strQuery);
             while(rs.next()) {
-                station.stationName = rs.getString("station_name");
-                station.stationCode = rs.getString("station_code");
-                station.stationDetailId = rs.getInt("station_detail_id");
-                station.lineId = rs.getInt("line_id");
-                station.type = rs.getInt("station_name");
-                station.stationName = rs.getString("station_name");
+                subwayData temp = new subwayData();
+                temp.stationName = rs.getString("station_name");
+                temp.stationCode = rs.getString("station_code");
+                temp.stationDetailId = rs.getInt("station_detail_id");
+                temp.lineId = rs.getInt("line_id");
+                temp.beforeStation = rs.getInt("before_station");
+                temp.nextStation = rs.getInt("next_station");
+                station.add(temp);
 
             }
             //System.out.println("DB 연결 성공");
@@ -81,19 +90,19 @@ class databaseManager {
     }
 }
 
-class user {
-
-}
-
 public class Metro_Navi {
     public static void main(String[] args) {
         databaseManager dbManager = new databaseManager();
-        subwayData subData = new subwayData();
+        ArrayList<subwayData> subData = new ArrayList<subwayData>();
         System.out.print("출발 역 입력 : ");
         Scanner input = new Scanner(System.in);
         String departureStation = input.next();
         subData = dbManager.getStationData(departureStation);
-        System.out.print(subData.stationDetailId);
+        int i = 0;
+        while( i < subData.size()) {
+            System.out.println(subData.get(i).stationDetailId);
+            i++;
         }
+    }
 }
 
